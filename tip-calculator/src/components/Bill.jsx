05550dadf9efb91ad2f0d9d427c83fedd19b0983
ramirez2303 <exeramirez23@gmail.com>
2022-04-13 05:11:10 -0300

    Modificate heading to text

--- tip-calculator/src/components/Bill.jsx
+++ tip-calculator/src/components/Bill.jsx
@@ -1,5 +1,5 @@
 import {
-  Heading,
+  Text,
   Image,
   Input,
   InputGroup,
@@ -7,23 +7,24 @@ import {
   Stack,
 } from "@chakra-ui/react";
 import React from "react";
-import dolar from "../image/icon-dollar.svg";
+import dolarIcon from "../image/icon-dollar.svg";
 
 function Bill() {
   return (
     <Stack spacing={1}>
-      <Heading fontSize="xs" color="primary.300">
+      <Text fontSize="xs" color="primary.300">
         Bill
-      </Heading>
+      </Text>
       <Stack>
         <InputGroup bgColor="gray.100" borderRadius="lg">
-          <InputLeftElement h="50px" children={<Image src={dolar} w={2} />} />
+          <InputLeftElement h="50px" children={<Image src={dolarIcon} w={2} />} />
           <Input
             h="50px"
             textAlign="right"
             type="number"
             color="primary.200"
             fontSize="sm"
+            placeholder="000.00"
             border="none"
             _active={{
               outline: "none",
